16c3ea005e73777055687dac6e8553ee25da6d70
Nikolay Mishcheriakov <deswars2011@gmail.com>
2021-08-04 19:36:05 +0300

    InfoLevels flag names

--- TradeMap.GameLog/InfoLevels.cs
+++ TradeMap.GameLog/InfoLevels.cs
@@ -1,14 +1,24 @@
 ﻿using System;
+using System.Runtime.Serialization;
 
 namespace TradeMap.GameLog
 {
     [Flags]
     public enum InfoLevels
     {
+        [EnumMember(Value = "None")]
         None = 0,
+
+        [EnumMember(Value = "Info")]
         Info = 1,
+
+        [EnumMember(Value = "Warn")]
         Warning = 2,
+
+        [EnumMember(Value = "Erro")]
         Error = 4,
+
+        [EnumMember(Value = "All")]
         All = 0xFFFF
     }
 }
